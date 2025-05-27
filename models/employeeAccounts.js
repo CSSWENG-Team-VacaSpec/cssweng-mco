623dff0000fafd8baa3f423cd79ef9ac919f3e7e
XENNNOOOO <jansen_mortega@dlsu.edu.ph>
2025-05-27 18:59:07 +0800

    [employeeAccounts] added bio field

--- models/employeeAccounts.js
+++ models/employeeAccounts.js
@@ -6,6 +6,7 @@ const employeeAccountSchema = new mongoose.Schema({
     firstName: { type: String, required: true }, 
     lastName: { type: String, required: true },
     password: { type: String, required: true },
+    bio: { type: String, required: false },
     pfp: {
         data: Buffer,
         contentType: String
