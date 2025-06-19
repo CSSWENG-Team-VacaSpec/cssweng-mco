e7ea5a1f25f76fe7e6b473b8adcfc06d946af727
jayy <73204320+jayylmao@users.noreply.github.com>
2025-06-19 23:34:03 +0800

    feat: mobile sidebar

--- public/common/javascript/sidebar.js
+++ public/common/javascript/sidebar.js
@@ -7,4 +7,42 @@ document.addEventListener('DOMContentLoaded', function() {
             link.classList.add('active');
         }
     });
+
+    // toggle mobile nav bar visibility.
+    let navIsVisible = false;
+    const sidebar = document.getElementById('global-sidebar');
+
+    document.getElementById('mobile-nav-button').addEventListener('click', function() {
+        navIsVisible = !navIsVisible;
+
+        if (navIsVisible) {
+            sidebar.style.opacity = 1;
+            sidebar.style.pointerEvents = 'all';
+            sidebar.style.marginBottom = 0;
+            sidebar.style.transform = 'scale(1)';
+        } else {
+            sidebar.style.opacity = 0;
+            sidebar.style.pointerEvents = 'none';
+            sidebar.style.marginBottom = '1rem';
+            sidebar.style.transform = 'scale(0.98)';
+        }
+    });
+
+    // toggle mobile nav menu/sidebar visibility on breakpoint.
+    const breakpoint = window.matchMedia('(max-width: 800px)');
+
+    breakpoint.addEventListener('change', function() {
+        // make sidebar visible when screen is wide enough.
+        if (breakpoint.matches) {
+            navIsVisible = true;
+            sidebar.style.opacity = 0;
+            sidebar.style.pointerEvents = 'none';
+            sidebar.style.zIndex = -1000;
+        } else { // hide sidebar by default when screen is too narrow.
+            navIsVisible = false;
+            sidebar.style.opacity = 1;
+            sidebar.style.pointerEvents = 'all';
+            sidebar.style.zIndex = 1000;
+        }
+    });
 });
