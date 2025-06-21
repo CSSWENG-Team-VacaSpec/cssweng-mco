a32de4ef90083f00540c30d75dd1f9dce8fb3e5b
d0sssss <aaron_armas@dlsu.edu.ph>
2025-06-21 21:06:46 +0800

    removed double declaration or route in server

--- main.js
+++ main.js
@@ -54,7 +54,6 @@ const searchBarRoute = require('./routes/r_searchBar');
 const eventListRoute = require('./routes/r_event_list.js');
 const notificationRoute = require('./routes/r_notifications.js');
 const eventDetailsRoute = require('./routes/r_event_details.js');
-const eventListRoute = require('./routes/r_event_list');
 
 // routes
 app.use('/', eventListRoute);
