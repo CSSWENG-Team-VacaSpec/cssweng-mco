820d0d3739af74cba99e853b3302903aaac48240
jayylmao <73204320+jayylmao@users.noreply.github.com>
2025-07-06 23:02:31 +0800

    feat: add page 1 of create events page

---
+++ public/common/javascript/eventCreate.js
@@ -0,0 +1,33 @@
+let page = 0;
+const MAX_PAGE = 2;
+
+document.addEventListener('DOMContentLoaded', () => {
+    const nextButton = document.getElementById('form-next-button');
+    const backButton = document.getElementById('form-back-button');
+
+    nextButton.addEventListener('click', () => {
+        if (page < 2) {
+            page++;
+        }
+        updateButtons();
+    });
+
+    backButton.addEventListener('click', () => {
+        if (page > 0) {
+            page--;
+        }
+        updateButtons();
+    });
+
+    function updateButtons() {
+        nextButton.disabled = page >= MAX_PAGE;
+        nextButton.classList.toggle('disabled-button', nextButton.disabled);
+        nextButton.classList.toggle('fg-button', !nextButton.disabled);
+
+        backButton.disabled = page <= 0;
+        backButton.classList.toggle('disabled-button', backButton.disabled);
+        backButton.classList.toggle('bg-button', !backButton.disabled);
+    }
+
+    updateButtons();
+});
