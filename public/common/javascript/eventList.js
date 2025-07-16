659b939e5fb45815f5a8e3998d4e73bcd2899b09
jayylmao <73204320+jayylmao@users.noreply.github.com>
2025-07-16 15:49:07 +0800

    feat: redid event list layout for better visual balance

--- public/common/javascript/eventList.js
+++ public/common/javascript/eventList.js
@@ -93,22 +93,35 @@ document.addEventListener('DOMContentLoaded', function() {
             container.appendChild(eventElement);
             
             eventElement.outerHTML = `
-                <div class="event-box container"
+                <div class="event-box card"
                     data-id="${event._id}"
                     data-cp-first-name="${event.CPFirstName}"
                     data-cp-last-name="${event.CPLastName}"
                     data-cp-contact-no="${event.CPContactNo}"
                 >
                     <div class="event-box-top">
-                        <span id="event-name">${event.eventName}</span>
-                        <span id="client-name">${event.clientFirstName} ${event.clientLastName}</span>
+                        <div class="main-event-info">
+                            <span class="card-title">${event.eventName}</span>
+                            <span id="client-name">${event.clientFirstName} ${event.clientLastName}</span>
+                        </div>
+                        <span id="status" data-status="${event.status}">${event.status}</span>
                     </div>
-                    <div class="event-box-bottom">
-                        <span id="date">${event.eventDate}</span>
-                        <span id="location">${event.location}</span>
-                        <span><strong>Description</strong></span>
-                        <span id="description">${event.description || 'No description'}</span>
-                        <span id="status" data-status="${event.status}"><strong>● Status:</strong> ${event.status}</span>
+                    <div class="card-group">
+                        <div class="card-description-item">
+                            <i class="lni lni-calendar-days"></i>
+                            <span id="date"> ${event.eventDate}</span>
+                        </div>
+                        <div class="card-description-item">
+                            <i class="lni lni-map-pin-5"></i>
+                            <span id="location">${event.location}</span>
+                        </div>
+                    </div>
+                    
+                    <div class="card-group">
+                        <div class="card-description-item">
+                            <i class="lni lni-pen-to-square"></i>
+                            <span id="description">${event.description || 'No description'}</span>
+                        </div>
                     </div>
                 </div>
             `;
