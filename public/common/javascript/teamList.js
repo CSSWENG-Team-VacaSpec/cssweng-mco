4d9eea1042cf77bdfed2a299064a95f41ae8a7d8
jayylmao <73204320+jayylmao@users.noreply.github.com>
2025-07-18 14:19:38 +0800

    feat: add controls to add and remove team members and suppliers in the team page

--- public/common/javascript/teamList.js
+++ public/common/javascript/teamList.js
@@ -4,4 +4,39 @@ document.addEventListener('DOMContentLoaded', function() {
         if (!teamMember) return;
 
     });
+
+    const addDropdown = document.getElementById('addDropdown');
+    const removeDropdown = document.getElementById('removeDropdown');
+
+    let addDropdownOpen = false;
+    let removeDropdownOpen = false;
+
+    document.addEventListener('click', (e) => {
+        if (!e.target.closest('#addDropdown')) {
+            addDropdownOpen = false;
+        }
+        
+        if (!e.target.closest('#removeDropdown')) {
+            removeDropdownOpen = false;
+        }
+        
+        updateDropdowns();
+    });
+
+    addDropdown.addEventListener('click', () => {
+        addDropdownOpen = !addDropdownOpen;
+        removeDropdownOpen = false;
+        updateDropdowns();
+    });
+    
+    removeDropdown.addEventListener('click', () => {
+        removeDropdownOpen = !removeDropdownOpen;
+        addDropdownOpen = false;
+        updateDropdowns();
+    });
+
+    function updateDropdowns() {
+        addDropdown.classList.toggle('dropdown-open', addDropdownOpen);
+        removeDropdown.classList.toggle('dropdown-open', removeDropdownOpen);
+    }
 });
