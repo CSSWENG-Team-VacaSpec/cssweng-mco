7d05e6adf08c0b91b706fc96578b577813bbbd03
jayy <73204320+jayylmao@users.noreply.github.com>
2025-07-20 21:21:03 +0800

    feat: add log out button

---
+++ controllers/profileController.js
@@ -0,0 +1,38 @@
+const EmployeeAccount = require('../models/employeeAccounts');
+
+exports.renderPage = async (req, res) => {
+    try {
+        if (!req.session.user) {
+            return res.redirect('/login');
+        }
+
+        const profile = await EmployeeAccount.findOne({
+            _id: req.params.id
+        }).lean();
+
+        // check if profile is your own or someone else's to determine
+        // the highlight on the sidebar.
+        let profileType = 'my-profile';
+
+        if (req.params.id !== req.session.user._id) {
+            profileType = 'other-profile'
+        }
+
+        console.log('viewing profile page of type ' + profileType + '\nid in params: ' + req.params.id + '\nid in session: ' + req.session._id);
+
+        res.render('profile', {
+            layout: 'main',
+            stylesheet: 'profile',
+            script: 'profile',
+            title: 'Profile', // TODO: change to user being displayed
+            page: profileType,
+            user: req.session.user,
+            profile: profile
+        });
+
+        console.log(profile);
+    } catch (error) {
+        console.error('Error loading profile page:', error);
+        res.status(500).send('Server Error: Unable to load profile.');
+    }
+};
