f1bbbd4dd78fa68603e598593d57da79e04bb617
jayy <73204320+jayylmao@users.noreply.github.com>
2025-07-25 22:38:26 +0800

    feat: add delete supplier page

--- public/common/javascript/memberDelete.js
+++ public/common/javascript/memberDelete.js
@@ -38,6 +38,5 @@ document.addEventListener('DOMContentLoaded', () => {
         
         deleteButton.disabled = addedMembers.length === 0;
         deleteButton.classList.toggle('disabled-button', deleteButton.disabled);
-        console.log(addedMembers);
     });
 });
