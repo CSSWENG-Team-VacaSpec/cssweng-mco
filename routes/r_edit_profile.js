90176936587c9faea5adfd6ce465cc8c545fc850
d0sssss <aaron_armas@dlsu.edu.ph>
2025-07-26 22:39:33 +0800

    edit profile/changepw done

--- routes/r_edit_profile.js
+++ routes/r_edit_profile.js
@@ -3,5 +3,8 @@ const router = express.Router();
 const controller = require('../controllers/profileEditController.js');
 
 router.get('/profile', controller.renderPage);
+router.post('/edit-profile-details', controller.editProfileDescription);
+router.post('/changePassword', controller.changePassword);
+
 
 module.exports = router;
