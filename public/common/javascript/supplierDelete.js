7171a2a74fd54d7fc0872e6e6bdf6211d83fa07e
XENNNOOOO <jansen_mortega@dlsu.edu.ph>
2025-07-27 15:39:14 +0800

    Revert "feat: remove members and suppliers"
    This reverts commit f83ac3a359c4147dd342b185ed238960fd104c5e.

--- public/common/javascript/supplierDelete.js
+++ public/common/javascript/supplierDelete.js
@@ -6,47 +6,36 @@ document.addEventListener('DOMContentLoaded', () => {
     const deleteButton = document.getElementById('deleteSuppliersButton');
     const suppliersContainer = document.getElementById('supplierSearchResults');
     const addedSuppliersContainer = document.getElementById('addedSuppliers');
-    const form = deleteButton.closest('form');
     let addedSuppliers = [];
 
     suppliersContainer.addEventListener('click', (event) => {
         const supplier = event.target.closest('.team-member-mini-card');
-        if (!supplier) return;
-
         let selected = supplier.classList.contains('selected-team-member');
         supplier.classList.toggle('selected-team-member', !selected);
         
         if (!selected) {
             const clone = supplier.cloneNode(true);
             addedSuppliers.push(supplier.dataset.id);
+            
+            // satore reference to clone on the original element
             supplier._cloneRef = clone;
-
+            
             clone.addEventListener('click', function() {
                 addedSuppliersContainer.removeChild(clone);
                 supplier.classList.remove('selected-team-member');
-                addedSuppliers = addedSuppliers.filter(id => id !== supplier.dataset.id);
             });
-
+            
             addedSuppliersContainer.appendChild(clone);
         } else {
+            // remove clone if exists when deselecting
             if (supplier._cloneRef && addedSuppliersContainer.contains(supplier._cloneRef)) {
                 addedSuppliersContainer.removeChild(supplier._cloneRef);
-                addedSuppliers = addedSuppliers.filter(id => id !== supplier.dataset.id);
+                addedSuppliers.pop(supplier.dataset.id);
                 supplier._cloneRef = null;
             }
         }
-
+        
         deleteButton.disabled = addedSuppliers.length === 0;
         deleteButton.classList.toggle('disabled-button', deleteButton.disabled);
-    });
-
-    form.addEventListener('submit', (e) => {
-        addedSuppliers.forEach(id => {
-            const input = document.createElement('input');
-            input.type = 'hidden';
-            input.name = 'supplierIds';
-            input.value = id;
-            form.appendChild(input);
-        });
     });
 });
