4561f6c2396510f1daf6ce811fccb0bc675042b7
jayy <73204320+jayylmao@users.noreply.github.com>
2025-07-31 20:54:03 +0800

    feat: add error message display

--- public/common/javascript/error.js
+++ public/common/javascript/error.js
@@ -1,9 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let error;
-    try {
-        error = document.querySelector('.error-container');
-        error.classList.add('error-show');
-    } catch (e) {
-
-    }
+    displayError();
 });
+
+function displayError(message="") {
+    let error;
+    const description = document.getElementById('errorDescription');
+    
+    // either display a specific message or display the one sent by backend.
+    if (message !== "") {
+        description.textContent = message;
+    } else {
+        if (description.textContent !== "") {
+            error = document.querySelector('.error-container');
+            error.classList.add('error-show');
+        }
+    }
+}
